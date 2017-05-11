d93c70812696250d7caa0524668a2ebd57a541e1
Rod Michael Coronel <rodmaykel@gmail.com>
2017-05-11 21:36:21 +0200

    answer array-sum-pairs

--- javascript/array-sum-pairs/index.js
+++ javascript/array-sum-pairs/index.js
@@ -1,3 +1,16 @@
 module.exports = function(array, sum) {
-
-};
+	for (var i = 1; i<array.length; i++) {
+		x = 0;
+		y = i;
+		while(y < array.length) {
+			//console.log(x + "," + y);
+			if (array[x] + array[y] == sum) {
+				return [array[x],array[y]];
+			}
+			x = x+1;
+			y = y+1;
+		}
+	}
+	return undefined;
+}
+;
